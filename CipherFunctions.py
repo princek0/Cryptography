36b9de09d864ff4f5d6c20a802b65e4fcf94da69
Prince <62904535+princek0@users.noreply.github.com>
2021-05-23 09:55:09 +0100

    Update CipherFunctions.py

--- CipherFunctions.py
+++ CipherFunctions.py
@@ -1,4 +1,5 @@
 def caesar(plain_text, key): # Caesar cipher function. 
+    plain_text = plain_text.lower()
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     for i in plain_text:
         if i == ' ': # Deals with spaces in the plain text.
