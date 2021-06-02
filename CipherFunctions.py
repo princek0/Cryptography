6bd49c1850af7b8dfaa42f903e56dafe39e65cfb
Prince <62904535+princek0@users.noreply.github.com>
2021-06-02 14:25:27 +0100

    Update CipherFunctions.py

--- CipherFunctions.py
+++ CipherFunctions.py
@@ -3,8 +3,8 @@ def caesar(plain_text, key):  # Caesar cipher function.
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
                 "v", "w", "x", "y", "z"]
     for i in plain_text:
-        if i == ' ':  # Deals with spaces in the plain text.
-            plain_text = plain_text.replace(' ', ' ')
+        if i not in alphabet:
+            pass
         else:
             plain_text = plain_text.replace(i, alphabet[
                 (alphabet.index(i) + key) % 26])  # Mod 26 needed to loop back around the alphabet.
@@ -16,8 +16,8 @@ def de_caesar(cipher_text, key):  # De-ciphers the cipher with a known key.
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
                 "v", "w", "x", "y", "z"]
     for i in cipher_text:
-        if i == ' ':  # Deals with spaces in the cipher text.
-            cipher_text = cipher_text.replace(' ', ' ')
+        if i not in alphabet:
+            pass
         else:
             cipher_text = cipher_text.replace(i, alphabet[
                 (alphabet.index(i) - key) % 26])  # Mod 26 needed to loop back around the alphabet.
